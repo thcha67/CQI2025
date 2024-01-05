eb74304357c9a8cd7f759ace7fd20502995af134
loicOlivier <93949230+loicOlivier@users.noreply.github.com>
2024-01-04 22:25:42 -0500

    firmware update

--- CQI_vacances_noel/src/main.cpp
+++ CQI_vacances_noel/src/main.cpp
@@ -3,14 +3,14 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-#define servoPin_pince D0
-#define servoPin_updown D1
-#define servoPin_flip D2
+#define servoPin1 D0
+#define servoPin2 D1
+#define servoPin3 D2
 
-#define motor1Pin1 D3
-#define motor1Pin2 D4
-#define motor2Pin1 D5
-#define motor2Pin2 D6
+#define motor1Pin1 D6
+#define motor1Pin2 D3
+#define motor2Pin1 D4
+#define motor2Pin2 D5
 typedef struct{
   String direction = "None";
   uint16_t speed = 0;
@@ -26,6 +26,8 @@ void setup_esp_wifi(const char *ssid, const char *password);
 void requestHandler(AsyncWebServerRequest *request);
 void request_getParam(AsyncWebServerRequest *request, command_t *command);
 
+void control(command_t *command);
+
 void initialize_servos(void);
 void command_servos(int16_t angle_pince, int16_t angle_updown, int16_t angle_flip);
 
@@ -40,9 +42,9 @@ void rotate_clock(void);
 void rotate_counter_clock(void);
 
 // instances de servos
-Servo servo_pince;
-Servo servo_updown;
-Servo servo_flip;
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
 const char *ssid = "Je suis Dieu et dispo";
 const char *password = "123456789";
@@ -155,22 +157,22 @@ void requestHandler(AsyncWebServerRequest *request){
 
 void initialize_servos(void)
 {
-  servo_pince.attach(servoPin_pince, 620, 2420);
-  servo_updown.attach(servoPin_updown, 620, 2420);
-  servo_flip.attach(servoPin_flip, 620, 2420);
+  servo1.attach(servoPin1, 620, 2420);
+  servo2.attach(servoPin2, 620, 2420);
+  servo3.attach(servoPin3, 620, 2420);
 
-  servo_pince.write(0);
-  servo_updown.write(0);
-  servo_flip.write(0);
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
 
 }
 
-void command_servos(int16_t angle_pince, int16_t angle_updown, int16_t angle_flip)
+void command_servos(const int16_t servo1_data, const int16_t servo2_data, const int16_t servo3_data)
 {
   
-  servo_pince.write(angle_pince);
-  servo_updown.write(angle_updown);
-  servo_flip.write(angle_flip);
+  servo1.write(servo1_data);
+  servo2.write(servo2_data);
+  servo3.write(servo3_data);
 }
 
 void initialize_motors(void){
@@ -192,16 +194,25 @@ void null_speed(void){
   analogWrite(motor2Pin2, 0);
 }
 
-void command_speed(int8_t speed, float correction){
-  if (speed > -20 and speed < 20){
+void command_speed_orientation(command_t *command){
+  if (command->direction == "None"){
     null_speed();
     return;
   }
-  else if (speed > 20){
-    forward(speed, correction);
+  else if (command->direction == "Forward"){
+    forward(command->speed, command->correction);
   }
-  else if (speed < 20){
-    backward(-speed, correction);
+  else if (command->direction == "Backward"){
+    backward(command->speed, command->correction);
+  }
+  else if(command->direction == "Right"){
+    rotate_clock();
+  }
+  else if(command->direction == "Left"){
+    rotate_counter_clock();
+  }
+  else{
+    null_speed();
   }
 }
 
@@ -210,11 +221,6 @@ void rotate_counter_clock(void){
   analogWrite(motor1Pin2, 255);
   analogWrite(motor2Pin1, 255);
   analogWrite(motor2Pin2, 0);
-
-  delay(200);
-
-  null_speed();
-
 }
 
 void rotate_clock(void){
@@ -321,4 +327,9 @@ void request_getParam(AsyncWebServerRequest *request, command_t *command){
     }
 
     
+}
+
+void control(command_t *command){
+  command_speed_orientation(command);
+  command_servos(command->servo1, command->servo2, command->servo3);
 }
